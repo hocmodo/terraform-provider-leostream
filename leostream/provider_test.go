dfeb922effae784efe50a09259d380d258478c12
Joost Evertse <joustie@gmail.com>
2024-10-11 18:03:35 +0200

    Added provider test

---
+++ leostream/provider_test.go
@@ -0,0 +1,26 @@
+package leostream
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+)
+
+const (
+	providerConfig = `
+provider "leostream" {
+  host     = "https://192.168.178.79"
+  username = "api"
+  password = "System@123"
+}
+`
+)
+
+var (
+	// testAccProtoV6ProviderFactories are used to instantiate a provider during
+	// acceptance testing. The factory function will be invoked for every Terraform
+	// CLI command executed to create a provider server to which the CLI can
+	// reattach.
+	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+		"leostream": providerserver.NewProtocol6WithError(New()),
+	}
+)
