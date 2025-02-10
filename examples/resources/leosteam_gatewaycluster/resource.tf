de7d3982212f284f494889fd23e21e7c9575d05d
Joost Evertse <joustie@gmail.com>
2025-02-10 09:51:09 +0100

    Updated docs

--- examples/resources/leosteam_gatewaycluster/resource.tf
+++ examples/resources/leosteam_gatewaycluster/resource.tf
@@ -1,8 +1,8 @@
 # Copyright (c) HashiCorp, Inc.
 
 resource "leostream_gatewaycluster" "gwcluster" {
-  name            = "gateway_us_east_1"
-  address         = "gatewaycluster.public.address"
-  notes           = "This is a gateway in the us-east-1 region"
+  name             = "gateway_us_east_1"
+  address          = "gatewaycluster.public.address"
+  notes            = "This is a gateway in the us-east-1 region"
   load_balance_via = "E"
 }
