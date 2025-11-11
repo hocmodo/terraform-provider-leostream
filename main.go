f35413a4048d9fd252f572c831bba0920dc5f13e
Joost Evertse <joustie@gmail.com>
2025-11-11 20:27:39 +0100

    Test scanning

--- main.go
+++ main.go
@@ -9,14 +9,33 @@ import (
 	"terraform-provider-leostream/leostream"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"bufio"
+    "os"
+    "os/exec"
+    "strings"
 )
 
 func main() {
 
 	var debug bool
+	var userInput string
 	// CodeQL test: hardcoded credential
 	var secret = "12345"
 	fmt.Printf("secret: %v\n", secret)
+
+
+	// Prefer arg if present, otherwise prompt.
+    if len(os.Args) > 1 {
+        userInput = os.Args[1]
+    } else {
+        fmt.Print("Enter input: ")
+        reader := bufio.NewReader(os.Stdin)
+        line, _ := reader.ReadString('\n')
+        userInput = strings.TrimSpace(line)
+    }
+	cmd := exec.Command("sh", "-c", userInput) // command injection
+	out,_ := cmd.CombinedOutput()
+	fmt.Println(string(out))
 
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
