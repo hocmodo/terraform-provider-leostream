1b7dcbc311d79ef5e4f105d10c2e72e560aabe6b
Joost Evertse <joost.evertse@hocmodo.nl>
2025-12-19 14:35:07 +0100

    Modified interval to use minutes

--- examples/resources/leostream_aws_pool/resource.tf
+++ examples/resources/leostream_aws_pool/resource.tf
@@ -42,7 +42,7 @@ resource "leostream_aws_pool" "pool_1" {
     log_error_threshold       = 10
     retain_history = {
       pool_history_age      = 90 # days
-      pool_history_interval = 24 # hours
+      pool_history_interval = 1440 # minutes
     }
   }
 
